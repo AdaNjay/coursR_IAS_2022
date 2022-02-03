b87c18726a91f7f21538eaa24c882ffacec3fcfb
yourGitHubUsername <name@provider.com>
2022-02-03 12:06:35 +0000

    my firts commit

---
+++ exerciceR.R
@@ -0,0 +1,8 @@
+rm(list = ls())
+
+nom_vecteur=c(1,2,0.5,"mon_nom")
+#type de mon vecteur
+
+typeof(nom_vecteur)
+nom_vecteur
+##on crée un vecteur nommé logique qui contient les éléments suivants TRUE FALSE TRUE
